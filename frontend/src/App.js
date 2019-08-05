3def21b1839769afb99c3010090909905792045d
Víctor Gabriel Moreno <victorgmp@gmail.com>
2019-08-05 15:51:32 -0400

    add frontend (create-react-app)

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import Navigation from './componets/Navigation';
+import NotesList from './componets/NotesList';
+import CreateNote from './componets/CreateNote';
+import CreateUser from './componets/CreateUser';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+
+        <Route path="/" exact component={NotesList} />
+        <Route path="/edit/id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
